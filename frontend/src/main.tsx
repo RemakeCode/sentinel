9f570ddacbf77ed500bc4f67450b263096ae2e70
afk-rightNow <258896796+afk-rightNow@users.noreply.github.com>
2026-03-04 21:24:46 +0100

    feat: Add smooth scroll to top on navigation across pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,14 @@
 import '@wailsio/runtime';
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createHashRouter, RouterProvider } from 'react-router';
+import { createHashRouter, RouterProvider, ScrollRestoration, useLocation } from 'react-router';
 import '@/shared/styles/global.scss';
 import '@knadh/oat/oat.min.js';
 import GameDetails from '@/pages/game-details/game-details';
 import Settings from '@/pages/settings/settings';
 import Dashboard from '@/pages/dashboard/dashboard';
+import { ScrollToTop } from '@/shared/components/ScrollToTop/scroll-to-top';
+import App from '@/app';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -14,15 +16,21 @@ const root = createRoot(container!);
 const router = createHashRouter([
   {
     path: '/',
-    element: <Dashboard />
-  },
-  {
-    path: '/game/:id',
-    element: <GameDetails />
-  },
-  {
-    path: '/settings',
-    element: <Settings />
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />
+      },
+      {
+        path: '/game/:id',
+        element: <GameDetails />
+      },
+      {
+        path: '/settings',
+        element: <Settings />
+      }
+    ]
   }
 ]);
 
