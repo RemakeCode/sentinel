2627993d2d1490df57addfeff040505c8df3b441
afk-rightNow <258896796+afk-rightNow@users.noreply.github.com>
2026-05-06 11:23:02 +0200

    feat: decky Plugin implementation

--- decky-plugin/src/common/components/img-icon.tsx
+++ decky-plugin/src/common/components/img-icon.tsx
@@ -2,9 +2,11 @@ import { IMG_URL } from '@/shared/utils/fetcher';
 import { CSSProperties } from 'react';
 
 const style: CSSProperties = {
-  width: '60px',
+  width: 'inherit',
+  height: 'inherit',
   border: '1px solid #3d4450',
-  borderRadius: '4px'
+  borderRadius: '4px',
+  marginLeft: '4px'
 };
 export const ImgIcon = ({ src }: { src: string }) => {
   return <img style={style} src={`${IMG_URL}${src}`} alt='' data-name='ach' />;
