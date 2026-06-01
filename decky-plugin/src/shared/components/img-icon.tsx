903f0b10cfad5c3234387f6dd04322524e816a0d
afk-rightNow <258896796+afk-rightNow@users.noreply.github.com>
2026-06-01 17:02:38 +0200

    refactor(decky-plugin): moved margin into the notification class where it is needed

--- decky-plugin/src/shared/components/img-icon.tsx
+++ decky-plugin/src/shared/components/img-icon.tsx
@@ -5,8 +5,7 @@ const style: CSSProperties = {
   width: 'inherit',
   height: 'inherit',
   border: '1px solid #3d4450',
-  borderRadius: '4px',
-  marginLeft: '4px'
+  borderRadius: '4px'
 };
 export const ImgIcon = ({ src }: { src: string }) => {
   return <img style={style} src={`${IMG_URL}${src}`} alt='' data-name='ach' />;
